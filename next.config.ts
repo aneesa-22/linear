cefd76172cea51eaa373aafb000d8009d08cb983
Asad Ishaq <asad@Asads-MacBook-Pro.local>
2026-05-30 20:23:35 -0400

    added config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  devIndicators: false,
+  allowedDevOrigins: [
+    "Asads-MacBook-Pro.local",
+    "Mac",
+    "192.168.0.4"
+  ],
+};
 
 export default nextConfig;
